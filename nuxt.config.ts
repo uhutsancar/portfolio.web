07ac456a08935626c4a43d3c7286cf2f4ec57f62
Uhut Sancar <sancaruhut@gmail.com>
2026-08-11 15:33:01 +0300

    rollback commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,119 +1,3 @@
-// // https://nuxt.com/docs/api/configuration/nuxt-config
-// export default defineNuxtConfig({
-//   compatibilityDate: '2025-07-15',
-//   css: ['~/assets/css/main.css'],
-
-//   app: {
-//     pageTransition: {
-//       name: 'page',
-//       mode: 'out-in',
-//     },
-//   },
-
-//   modules: [
-//     '@nuxt/ui',
-//     '@nuxt/image',
-//     '@nuxt/content',
-//     'motion-v/nuxt',
-//     'nuxt-studio',
-//   ],
-
-//   studio: {
-//     repository: {
-//       provider: 'github',
-//       owner: 'uhutsancar',
-//       repo: 'portfolio.web',
-//       branch: 'main',
-//     },
-
-//     git: {
-//       commit: {
-//         messagePrefix: 'content:',
-//       },
-//     },
-
-//     editor: {
-//       iconLibraries: [
-//         'lucide',
-//         'simple-icons',
-//       ],
-//     },
-//   },
-
-//   content: {
-//     experimental: {
-//       sqliteConnector: 'native',
-//     },
-//   },
-
-//   image: {
-//     format: ['webp', 'avif'],
-//     quality: 80,
-//     screens: {
-//       xs: 320,
-//       sm: 640,
-//       md: 768,
-//       lg: 1024,
-//       xl: 1280,
-//     },
-//   },
-
-//   nitro: {
-//     routeRules: {
-//       '/sitemap.xml': {
-//         headers: {
-//           'Cache-Control': 'public, max-age=86400',
-//         },
-//       },
-
-//       '/robots.txt': {
-//         headers: {
-//           'Cache-Control': 'public, max-age=86400',
-//         },
-//       },
-
-//       '/': {
-//         isr: 3600,
-//       },
-
-//       '/about': {
-//         isr: 3600,
-//       },
-
-//       '/projects': {
-//         isr: 3600,
-//       },
-
-//       '/blog': {
-//         isr: 3600,
-//       },
-
-//       '/speaking': {
-//         isr: 3600,
-//       },
-
-//       '/contact': {
-//         isr: 3600,
-//       },
-//     },
-//   },
-
-//   runtimeConfig: {
-//     resend: {
-//       apiKey: '',
-//       from: '',
-//       to: '',
-//     },
-//   },
-// })
-
-
-
-
-
-
-
-
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
@@ -181,26 +65,33 @@ export default defineNuxtConfig({
           'Cache-Control': 'public, max-age=86400',
         },
       },
+
       '/robots.txt': {
         headers: {
           'Cache-Control': 'public, max-age=86400',
         },
       },
+
       '/': {
         isr: 3600,
       },
+
       '/about': {
         isr: 3600,
       },
+
       '/projects': {
         isr: 3600,
       },
+
       '/blog': {
         isr: 3600,
       },
+
       '/speaking': {
         isr: 3600,
       },
+
       '/contact': {
         isr: 3600,
       },
@@ -213,17 +104,15 @@ export default defineNuxtConfig({
       from: '',
       to: '',
     },
-    session: {
-      password: '',
-    },
-    oauth: {
-      github: {
-        clientId: '',
-        clientSecret: '',
-      },
-    },
   },
 })
+
+
+
+
+
+
+
 
 
 
